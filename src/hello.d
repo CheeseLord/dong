00742776a12846a37b94315360cd7a960df9984b
Greg Kronmiller <greg_kronmiller@hmc.edu>
2015-06-06 15:48:20 -0700

    Created SDL example, which may or may not work.

--- src/hello.d
+++ src/hello.d
@@ -1,7 +1,42 @@
 import std.stdio;
+import derelict.sdl2.sdl;
 
-void main()
+int main()
 {
-    writefln("Hello, world!");
+    DerelictSDL2.load();
+
+    SDL_Window *window;
+
+    debug writefln("1");
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    debug writefln("2");
+
+    window = SDL_CreateWindow("Dong", SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
+
+    debug writefln("3");
+
+    if (window == null) {
+        writefln("Oh noes! Failed to create window.");
+        return 1;
+    }
+
+    debug writefln("4");
+
+    SDL_Delay(3000);
+
+    debug writefln("5");
+
+    SDL_DestroyWindow(window);
+
+    debug writefln("6");
+
+    SDL_Quit();
+
+    debug writefln("7");
+
+    return 0;
 }
 
