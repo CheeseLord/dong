dd39984af8631191423fe68d4695a52d1ed02d23
Greg Kronmiller <greg_kronmiller@hmc.edu>
2015-06-13 15:44:13 -0700

    We now print event names.
    At least, as long as it's a keyboard or mouse event.

--- src/main.d
+++ src/main.d
@@ -39,7 +39,8 @@ void main()
         // FIXME: Do stuff here.
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
-            writefln("Got an event. Type = %s", event.type);
+            writefln("Got an event. Type = %s (%s)", event.type,
+                GetEventTypeName(event.type));
             if (event.type == SDL_QUIT) {
                 break MAIN_LOOP;
             }
@@ -51,6 +52,22 @@ void main()
         Duration elapsed = end - start;
 
         Thread.sleep(frameLength - elapsed);
+    }
+}
+
+string GetEventTypeName(uint eventType)
+{
+    switch(eventType) {
+        case SDL_QUIT:              return "quit";
+        case SDL_KEYDOWN:           return "key down";
+        case SDL_KEYUP:             return "key up";
+        case SDL_TEXTEDITING:       return "text editing";
+        case SDL_TEXTINPUT:         return "text input";
+        case SDL_MOUSEMOTION:       return "mouse motion";
+        case SDL_MOUSEBUTTONDOWN:   return "mouse button down";
+        case SDL_MOUSEBUTTONUP:     return "mouse button up";
+        case SDL_MOUSEWHEEL:        return "mouse wheel";
+        default:                    return "<something else>";
     }
 }
 
