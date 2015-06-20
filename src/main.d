e61506a6594df055a2a7272e2e93d33c4894a7bb
Greg Kronmiller <greg_kronmiller@hmc.edu>
2015-06-20 16:26:43 -0700

    Moved UpdateWorld to physics.d

--- src/main.d
+++ src/main.d
@@ -38,7 +38,7 @@ void main()
 
         // Update the game state, based on the amount of time elapsed since
         // the previous event loop iteration.
-        UpdateGame(currStartTime - prevStartTime);
+        UpdateWorld(currStartTime - prevStartTime);
 
         // Draw the current game state.
         RenderGame();
@@ -54,25 +54,4 @@ void main()
     }
 }
 
-
-void UpdateGame(Duration elapsedTime)
-{
-    // Convert the elapsedTime to seconds.
-    long secs, nsecs;
-    elapsedTime.split!("seconds", "nsecs")(secs, nsecs);
-    double elapsedSeconds = cast(double)(secs) + cast(double)(nsecs) / 1.0e9;
-
-    debug {
-        writefln("Updating game. %s.%07s seconds elapsed.", secs, nsecs / 100);
-    }
-
-    // Move the ball, based on its current velocity.
-    gameState.ball.x += gameState.ball.xVel * elapsedSeconds;
-    gameState.ball.y += gameState.ball.yVel * elapsedSeconds;
-
-    debug {
-        writefln("    Ball is at (%s, %s).",
-                 gameState.ball.x, gameState.ball.y);
-    }
-}
 
--- src/physics.d
+++ src/physics.d
@@ -1,3 +1,27 @@
+import std.stdio;
+import core.time;
+
 // We need to access entities' states to move them around in the world.
 import gamestate;
+
+void UpdateWorld(Duration elapsedTime)
+{
+    // Convert the elapsedTime to seconds.
+    long secs, nsecs;
+    elapsedTime.split!("seconds", "nsecs")(secs, nsecs);
+    double elapsedSeconds = cast(double)(secs) + cast(double)(nsecs) / 1.0e9;
+
+    debug {
+        writefln("Updating game. %s.%07s seconds elapsed.", secs, nsecs / 100);
+    }
+
+    // Move the ball, based on its current velocity.
+    gameState.ball.x += gameState.ball.xVel * elapsedSeconds;
+    gameState.ball.y += gameState.ball.yVel * elapsedSeconds;
+
+    debug {
+        writefln("    Ball is at (%s, %s).",
+                 gameState.ball.x, gameState.ball.y);
+    }
+}
 
