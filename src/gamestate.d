1507c3a136d0f1e6cda4a5e3c9c9dcb5bca0ecdb
Greg Kronmiller <greg_kronmiller@hmc.edu>
2015-06-27 17:39:34 -0700

    Added broken code to bounce ball off right wall.

--- src/gamestate.d
+++ src/gamestate.d
@@ -3,6 +3,13 @@ import core.time;
 
 import entity;
 
+// I suppose we could use this in the WorldRect struct, but I don't really want
+// to add more indirection there.
+struct WorldPoint {
+    double x;
+    double y;
+}
+
 struct WorldRect {
     double x;
     double y;
@@ -106,6 +113,39 @@ void UpdateWorld(Duration elapsedTime)
             }
         }
     }
+}
+
+/**
+ * Check whether two line segments intersect. One segment goes from s1Start to
+ * s1End; the other goes from s2Start to s2End. The second one must be
+ * perfectly vertical.
+ */
+bool SegmentIntersectsVerticalSegment(WorldPoint s1Start, WorldPoint s1End,
+                                      WorldPoint s2Start, WorldPoint s2End)
+{
+    // s2Start.x and s2End.x are assumed equal.
+    double s2x = s2Start.x;
+
+    if     ((s1Start.x < s2x && s1End.x > s2x) ||
+            (s1Start.x > s2x && s1End.x < s2x)) {
+        // Segment 1 starts and ends on opposite sides (horizontally speaking)
+        // of segment 2, so an intersection is possible.
+
+        // Compute the y-coordinate of segment 1 at s2x. If the two segments
+        // intersect, it must be at this y-coordinate, so we call it
+        // intersectY. To find this coordinate, we make use of the fact that:
+        //      intersectY - s1Start.y       s1End.y - s1Start.y
+        //     ------------------------  =  ---------------------
+        //             s2x - s1Start.x       s1End.x - s1Start.x
+        double intersectY = (s1End.y - s1Start.y) / (s1End.x - s1Start.x) *
+                            (s2x - s2Start.x) +
+                            s1Start.y;
+
+        return ((s1Start.y < intersectY && intersectY < s1End.y) ||
+                (s1Start.y > intersectY && intersectY > s1End.y));
+    }
+
+    return false;
 }
 
 WorldRect CenteredWRect(double centerX, double centerY, double w, double h)
--- src/physics.d
+++ src/physics.d
@@ -1,4 +1,5 @@
 import std.stdio;
+import std.math: abs;
 
 // We need to access entities' states to move them around in the world.
 import gamestate;
@@ -26,6 +27,43 @@ class BallPhysics : PhysicsComponent {
         super(parent);
 
         debug writefln("Constructing the Ball's PhysicsComponent.");
+    }
+
+    override void update(double elapsedTime)
+    {
+        double oldX = parent_.x;
+        double oldY = parent_.y;
+        super.update(elapsedTime);
+
+        bool finishedBouncing = false;
+
+        while (!finishedBouncing) {
+            foreach (Entity entity; gameState.entities) {
+                if (entity.bounceDir == BounceDirection.LEFT) {
+                    debug writefln("Checking for bounce.");
+                    if (SegmentIntersectsVerticalSegment(
+                                WorldPoint(oldX,      oldY),
+                                WorldPoint(parent_.x, parent_.y),
+                                WorldPoint(entity.x,  entity.y),
+                                WorldPoint(entity.x + entity.w,
+                                           entity.y + entity.h)
+                            )) {
+                        debug writefln("    Bouncing.");
+                        // parent_.right = entity.left -
+                        //                 abs(parent_.right - entity.left)
+                        parent_.x = entity.x -
+                                    abs((parent_.x + parent_.w) - entity.x) -
+                                    parent_.w;
+                        parent_.xVel = - parent_.xVel;
+                    }
+                }
+
+                // TODO: More directions
+            }
+
+            // TODO: Actually check this
+            finishedBouncing = true;
+        }
     }
 }
 
