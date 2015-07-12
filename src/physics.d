7c9e5fe8cbc0ecd7f6472db036070146cf2193db
Greg Kronmiller <greg_kronmiller@hmc.edu>
2015-07-11 18:53:42 -0700

    Misc cleanups to MaybeBounce.
    Add comments, rename some variables. Change the way it's called to
    remove the "directionName" parameter (which was only there for a debug
    writefln anyway).
    
    Cleaned up the code that calls MaybeBounce so that it actually checks
    whether there was a bounce. This is a first step toward correctly
    handling multiple bounces in a single frame, but we still don't handle
    that, as noted in a FIXME in MaybeBounce.

--- src/physics.d
+++ src/physics.d
@@ -47,70 +47,133 @@ class BallPhysics : PhysicsComponent {
         bool finishedBouncing = false;
 
         while (!finishedBouncing) {
+            finishedBouncing = true;
+
             foreach (Entity entity; gameState.entities) {
                 if (entity.bounceDir == BounceDirection.LEFT) {
-                    MaybeBounce!("right",  "left",   false, true,  "left")
-                                (parent_, entity, oldWRect);
+                    if (MaybeBounce!("right",  "left",   false, true)
+                                    (parent_, entity, oldWRect)) {
+                        debug writefln("    Bouncing left.");
+                        finishedBouncing = false;
+                        break;
+                    }
                 }
                 else if (entity.bounceDir == BounceDirection.RIGHT) {
-                    MaybeBounce!("left",   "right",  false, false, "right")
-                                (parent_, entity, oldWRect);
+                    if (MaybeBounce!("left",   "right",  false, false)
+                                    (parent_, entity, oldWRect)) {
+                        debug writefln("    Bouncing right.");
+                        finishedBouncing = false;
+                        break;
+                    }
                 }
                 else if (entity.bounceDir == BounceDirection.UP) {
-                    MaybeBounce!("bottom", "top",    true,  true,  "up")
-                                (parent_, entity, oldWRect);
+                    if (MaybeBounce!("bottom", "top",    true,  true)
+                                    (parent_, entity, oldWRect)) {
+                        debug writefln("    Bouncing up.");
+                        finishedBouncing = false;
+                        break;
+                    }
                 }
                 else if (entity.bounceDir == BounceDirection.DOWN) {
-                    MaybeBounce!("top",    "bottom", true,  false, "down")
-                                (parent_, entity, oldWRect);
+                    if (MaybeBounce!("top",    "bottom", true,  false)
+                                    (parent_, entity, oldWRect)) {
+                        debug writefln("    Bouncing down.");
+                        finishedBouncing = false;
+                        break;
+                    }
                 }
             }
-
-            // TODO: Actually check this
-            finishedBouncing = true;
         }
     }
 }
 
-bool MaybeBounce(string myEdgeName, string wallEdgeName, bool isVertical,
-                 bool isNegative, string directionName)
+/**
+ * Templated function that checks whether one Entity ("me") collides with
+ * another ("wall"), and if so reflects me off of wall in a particular
+ * direction.
+ * Template parameters:
+ *     myEdge -- The edge of me that will hit the wall first. For example, if
+ *         we're checking for collisions with the top wall, myEdge should be
+ *         "top".
+ *     wallEdge -- The edge of wall that me will hit first. For example, if
+ *         we're checking for collisions with the top wall, myEdge should be
+ *         "bottom".
+ *     isVertical -- true if this is a vertical collision (that is, if we will
+ *         be reflected upward or downward); false otherwise (that is, if we
+ *         will be reflected leftward or rightward).
+ *     isNegative -- true if the relevant component of me's velocity, after
+ *         it bounces (assuming that it does bounce), will be negative (that
+ *         is, if this is an upward or leftward bounce); false otherwise.
+ * Function parameters:
+ *     me -- The entity that may be bouncing. me.wRect should be the position
+ *         that me will have assuming it does *not* bounce off of the wall.
+ *     wall -- The entity that me may bounce off of.
+ *     myOldRect -- The position of me at the start of this frame.
+ * Returns true if me bounces off wall, false otherwise.
+ */
+bool MaybeBounce(string myEdge, string wallEdge, bool isVertical,
+                 bool isNegative)
                 (Entity me, Entity wall, WorldRect myOldRect)
 {
-    enum string myEdge   = "me."   ~ myEdgeName;
-    enum string wallEdge = "wall." ~ wallEdgeName;
+    // TODO: I couldn't figure out a way to create compile-time aliases to
+    // these (short of taking their addresses and storing them in pointers), so
+    // for now we're just calling mixin on them every time we need to use them.
+    // This isn't really the best thing.
+    enum string myEdgeCoord   = "me."   ~ myEdge;
+    enum string wallEdgeCoord = "wall." ~ wallEdge;
 
-    enum string myNewCorner1 = "me."        ~ GetEdgeCorners!myEdgeName  .corner1;
-    enum string myNewCorner2 = "me."        ~ GetEdgeCorners!myEdgeName  .corner2;
-    enum string myOldCorner1 = "myOldRect." ~ GetEdgeCorners!myEdgeName  .corner1;
-    enum string myOldCorner2 = "myOldRect." ~ GetEdgeCorners!myEdgeName  .corner2;
-    enum string wallCorner1  = "wall."      ~ GetEdgeCorners!wallEdgeName.corner1;
-    enum string wallCorner2  = "wall."      ~ GetEdgeCorners!wallEdgeName.corner2;
+    enum string myNewCorner1 = "me."        ~ GetEdgeCorners!myEdge  .corner1;
+    enum string myNewCorner2 = "me."        ~ GetEdgeCorners!myEdge  .corner2;
+    enum string myOldCorner1 = "myOldRect." ~ GetEdgeCorners!myEdge  .corner1;
+    enum string myOldCorner2 = "myOldRect." ~ GetEdgeCorners!myEdge  .corner2;
+    enum string wallCorner1  = "wall."      ~ GetEdgeCorners!wallEdge.corner1;
+    enum string wallCorner2  = "wall."      ~ GetEdgeCorners!wallEdge.corner2;
 
-    // TODO: Explain reversal of Intersects.
+    // Determine the two things that depend on whether this is a vertical
+    // bounce or a horizontal bounce:
+    //  1. Component of our velocity that should be inverted: x if horizontal,
+    //     y if vertical.
+    //  2. The function used to check whether a line segment (from old position
+    //     to new position) intersects the wall. Note that the edge of the wall
+    //     that we bounce off of is perpendicular to the direction of the
+    //     bounce: for example, if we're bouncing horizontally, then we must
+    //     therefore be bouncing off of a *vertical* edge of a wall.
     static if (isVertical) {
+        enum string myVel = "me.yVel";
         alias Intersects = SegmentIntersectsHorizontal;
     }
     else {
+        enum string myVel = "me.xVel";
         alias Intersects = SegmentIntersectsVertical;
     }
 
-    if (    Intersects(mixin(myOldCorner1), mixin(myNewCorner1), mixin(wallCorner1), mixin(wallCorner2)) ||
-            Intersects(mixin(myOldCorner2), mixin(myNewCorner2), mixin(wallCorner1), mixin(wallCorner2))) {
-        debug writefln("    Bouncing %s.", directionName);
+    // Check whether our displacement vector passes through the wall.
+    if (    Intersects(mixin(myOldCorner1), mixin(myNewCorner1),
+                       mixin(wallCorner1),  mixin(wallCorner2)) ||
+            Intersects(mixin(myOldCorner2), mixin(myNewCorner2),
+                       mixin(wallCorner1), mixin(wallCorner2))) {
 
+        // Use abs() to make sure we bounce in the right direction. If
+        // isNegative, then our final edge coordinate should be less than the
+        // wall's edge coordinate (we should be left of or above the wall);
+        // otherwise, our final edge coordinate should be greater than that
+        // of the wall (we should be to the right of or below the wall).
         static if (isNegative) {
-            mixin(myEdge) = mixin(wallEdge) - abs(mixin(myEdge)   - mixin(wallEdge));
+            mixin(myEdgeCoord) = mixin(wallEdgeCoord) -
+                abs(mixin(myEdgeCoord)   - mixin(wallEdgeCoord));
         }
         else {
-            mixin(myEdge) = mixin(wallEdge) + abs(mixin(wallEdge) - mixin(myEdge));
+            mixin(myEdgeCoord) = mixin(wallEdgeCoord) +
+                abs(mixin(wallEdgeCoord) - mixin(myEdgeCoord)  );
         }
 
-        static if (isVertical) {
-            me.yVel = - me.yVel;
-        }
-        else {
-            me.xVel = - me.xVel;
-        }
+        // FIXME: Also need to update myOldRect (which we currently can't do
+        // because it's passed by value) to our position just after the bounce,
+        // so that we can detect any further collisions (in case there are many
+        // collisions in a single frame).
+
+        // Actually start moving in the other direction.
+        mixin(myVel) = - mixin(myVel);
 
         return true;
     }
