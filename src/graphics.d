731293034ec8cd4956abfb287b3134184c1bcaa9
CheeseLord <fox_kyle@yahoo.com>
2015-08-15 20:30:25 -0400

    Started code for displaying scores.

--- src/graphics.d
+++ src/graphics.d
@@ -1,25 +1,35 @@
 import std.stdio;
+import std.conv;
 
 // For drawing.
 import derelict.sdl2.sdl;
+import derelict.sdl2.ttf;
 
 // We need to read entities' states to draw them.
 import gamestate;
 import entity;
 import worldgeometry;
+import score;
 
 alias ScreenRect = SDL_Rect;
 
 private SDL_Window  *window  = null;
 private SDL_Surface *surface = null;
 
+private TTF_Font *font;
+
+private SDL_Surface *leftScore = null;
+private SDL_Surface *rightScore;
+
 private ScreenRect sWorldRect;
 
 void InitGraphics()
 {
-    // Set up SDL.
+    // Set up SDL and TTF.
     DerelictSDL2.load();
     SDL_Init(SDL_INIT_VIDEO);
+    DerelictSDL2ttf.load();
+    TTF_Init();
 
     // Try creating a window.
     // FIXME: Remove magic numbers.
@@ -47,6 +57,10 @@ void InitGraphics()
         cast(int) surface.w, // w
         cast(int) surface.h  // h
     );
+
+    // Set up the font.
+    // TODO: Get default fonts.
+    font = TTF_OpenFont("fonts/UbuntuMono-B.ttf", 96);
 }
 
 void CleanupGraphics()
@@ -56,12 +70,15 @@ void CleanupGraphics()
     }
 
     writefln("Exiting.");
+    TTF_Quit();
     SDL_Quit();
 }
 
 void RenderGame()
 {
-    SDL_FillRect(surface, null, SDL_MapRGB(surface.format, 0, 0,   0));
+    SDL_FillRect(surface, null, SDL_MapRGB(surface.format, 0, 0, 0));
+
+    RenderScores();
 
     foreach (Entity entity; gameState.entities) {
         ScreenRect sEntityRect = WorldToScreenRect(entity.wRect, sWorldRect);
@@ -72,6 +89,16 @@ void RenderGame()
 
     // Update the window to actually display the newly drawn game state.
     SDL_UpdateWindowSurface(window);
+}
+
+void RenderScores()
+{
+    SDL_Color scoreColor = {255, 255, 255};
+    leftScore  = TTF_RenderText_Solid(font, to!string(scores.left).ptr,
+                                     scoreColor);
+    rightScore = TTF_RenderText_Solid(font, to!string(scores.right).ptr,
+                                     scoreColor);
+    // FIXME: Actually display.
 }
 
 
